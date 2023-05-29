63e141408aafd2680401ba061db5b31a0cca8c6c
Bug Coding <61137331+fawait123@users.noreply.github.com>
2023-05-29 23:47:56 +0700

    update teerbaru

--- Controllers/Auth/index.js
+++ Controllers/Auth/index.js
@@ -1,8 +1,78 @@
+const sha256 = require("js-sha256");
+const { v4: uuidv4 } = require("uuid");
+const Model = require("./../../models");
+const LoginSchema = require("../../schema/LoginSchema");
+const JWT = require("../../utils/jwt");
+
 const Auth = {
+  register: async (req, res) => {
+    try {
+      const body = req.body;
+
+      const { error, value } = LoginSchema.validate({ ...body });
+
+      if (error) {
+        return res.sendData(400, "validation errrors", error);
+      }
+
+      let hash = sha256.create();
+
+      hash.update(body.password);
+
+      const user = await Model.User.findOne({
+        where: {
+          username: body.username,
+        },
+      });
+
+      if (user) {
+        return res.sendData(400, "Akun sudah digunakan");
+      }
+
+      await Model.User.create({
+        id: uuidv4(),
+        username: body.username,
+        password: "$" + hash.hex(),
+        email: body.email,
+        name: body.name,
+      }).then((response) => {
+        return res.sendData(200, "success", response);
+      });
+    } catch (error) {
+      return res.sendData(500, error.message);
+    }
+  },
   login: async (req, res) => {
     try {
-      return res.send({ msg: "route login" });
-    } catch (error) {}
+      const body = req.body;
+
+      const hash = sha256.create();
+
+      hash.update(body.password);
+
+      const user = await Model.User.findOne({
+        where: {
+          username: body.username,
+        },
+      });
+
+      if (!user) {
+        return res.sendData(404, "Username atau password tidak ditemukan");
+      }
+
+      const hashPassword = "$" + hash.hex();
+      if (hashPassword != user.password) {
+        return res.sendData(404, "Username atau password tidak ditemukan");
+      }
+
+      let token = JWT.login(user);
+
+      delete user.dataValues.password;
+
+      return res.sendData(200, "success", { token, user });
+    } catch (error) {
+      return res.sendData(500, error.message);
+    }
   },
 };
 
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParse = require("body-parser");
 const Router = require("./Router/public");
+const decorators = require("./libraries/decorator");
 require("dotenv").config();
 
 const app = express();
@@ -9,9 +10,16 @@ const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParse.urlencoded({ extended: false }));
+app.use(bodyParse.json());
+
+decorators(app);
 
 // public route
 app.use("/", Router);
+
+app.use((req, res, next) => {
+  return res.sendNotFound();
+});
 
 app.listen(PORT, () => {
   console.log("App listening on http://localhost:" + PORT);
